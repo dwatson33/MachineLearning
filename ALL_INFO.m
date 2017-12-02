3e01e73bf7960868feb9d3bb5df4f444869b471c
dwatson33 <31700664+dwatson33@users.noreply.github.com>
2017-12-02 11:44:48 -0600

    Add files via upload

---
+++ ALL_INFO.m
@@ -0,0 +1,58 @@
+% This file contains information on all documnets.
+
+% atam.topwords has the information taken from the the study my work is
+% based off.
+% It has in column one the tweet ID for, cloumn two has the tweets info on
+% what it is, G is general, S is symptom, and T is treatment, column three
+% has the actual tweet, and column 4 has the weight of the tweet.
+
+% Calculating (1-6) are all my funtions used to determine the weight of the
+% tweet.
+% For my final project I used Calculating4.
+
+% Cases was used to determine how many splits the descision tree should
+% have, which was 6 splits, due to it having the highest success rate.
+
+% DataGrapher was an earlier version of my project and is actually used for
+% parts of Calculating.
+
+% Exces data is a file used that I just used to help determine which
+% Calculating to use, by comparing results.
+
+% Graphing and it's following 1-All are plots of the data, that I used so I
+% could see how the data was grapher and how the data should appear if it
+% was all labeled correctly. 
+
+% GraphingFinal wa used to compare the graphs of what they should be vs.
+% what it came out with when using the descision tree.
+
+% Labelstest and Labelstrain are the proper labels for my data.
+% test is obviously what the labels for my 60 test examples and train is my
+% labels for my 40 train examples.
+
+% Project contians the final code for my project and descriptions of the
+% files I used.
+
+% Testing, Testingagain, TestingIT, & TestingTesting123 are all files that
+% I used to test out different parts of my project, so that I didn't have
+% to do all at once and could test quickly.
+% They were also used to get out the bugs in the code and errors.
+
+% Tweets (1-6) were the spliting of my 100 tweets and their weights gotten
+% from plugging them into their respective Calculating functions.
+% Tweets4 is the Tweets weight I used for my final project, with
+% Calculating4.
+
+% TweetsTesting was also an extra testing file used to work out errors.
+
+% Values files contain the tweets total word count and the tweets weight
+% from the respective Tweets and Calculating files.
+% This was placed into the descision tree function and determined and used
+% to label the outcome of the tweets.
+% The Valuestest and Valuestrain were the final values used with the
+% project
+
+
+
+
+
--- Project.m
+++ Project.m
@@ -28,35 +28,39 @@ clear; clc
 % I believe if I were to continue varying the weights I could find a
 % perfect balance. 
 
-
+% Brings in the values of the tweets sick or not sick
 labels = 'Labelstrain.xlsx';
 [num, txt, raw] = xlsread(labels);
 SN = txt(:,1);
+% brings in the tweets word count and weight, determined from Tweets4,
+% which used Calculating4 to do this
 values = 'Valuestrain.xlsx';
 [num, txt, raw] = xlsread(values);
 V = num;
 
 tree = fitctree(V,SN);
+% Used to determine a descision tree for my data
 
 MdlDefault = fitctree(V,SN,'CrossVal','on');
-
 view(MdlDefault.Trained{6},'Mode','graph')
-Mdl7 = fitctree(V,SN,'MaxNumSplits',6,'CrossVal','on');
-classError6 = kfoldLoss(Mdl6)
+% Used to split the descion tree into 6, instead of 10
+
+%error of tree
+classError6 = kfoldLoss(MdlDefault)
 testvalues = 'Valuestest.xlsx';
 [num, txt, raw] = xlsread(testvalues);
 TV = num;
 
 label6 = predict(MdlDefault.Trained{6},TV);
-
-
+% labels given data to how the descion tree has split up the train data
 labeled = 'Labelstest.xlsx';
 [num, txt, raw] = xlsread(labeled);
 SNT = txt(:,1);
 
 correctness6 = strcmp(SNT,label6);
 correct6 = sum(correctness6);
-
+% out of 60
+error = 1-correct6/60
 
 
 %Split of 6 seems to be the best to get the most correct results out of
